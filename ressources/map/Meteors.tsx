ab857ba6f0e8bec73497ddf5456dc93d2b0ddddd
Sébastien Gonzalez <gonzo222@hotmail.fr>
2018-12-11 22:05:39 +0100

    Résolution bug map lors de l'export du jar

--- ressources/map/Meteors.tsx
+++ ressources/map/Meteors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="Spritesheet" tilewidth="120" tileheight="98" tilecount="18" columns="9">
- <image source="../sprites/Meteors/Spritesheet.png" width="1080" height="196"/>
+ <image source="test/Spritesheet.png" width="1080" height="196"/>
 </tileset>
